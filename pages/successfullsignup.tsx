ba486713701e7df3dff60e9333ee2a63b87c1196
Harsimran Preet Singh <harsimranpsingh@gmail.com>
2023-05-26 21:01:03 -0700

    yarn build update

--- pages/successfullsignup.tsx
+++ pages/successfullsignup.tsx
@@ -41,57 +41,57 @@ import ReactCanvasConfetti from "react-canvas-confetti";
 const variant_color = "blue";
 
 function Login() {
-    const refAnimationInstance = useRef(null);
+    // const refAnimationInstance = useRef(null);
 
-    const getInstance = useCallback((instance) => {
-      refAnimationInstance.current = instance;
-    }, []);
+    // const getInstance = useCallback((instance: null) => {
+    //   refAnimationInstance.current = instance;
+    // }, []);
 
-    const makeShot = useCallback((particleRatio, opts) => {
-      refAnimationInstance.current &&
-        refAnimationInstance.current({
-          ...opts,
-          origin: { y: 0.7 },
-          particleCount: Math.floor(200 * particleRatio),
-        });
-    }, []);
+    // const makeShot = useCallback((particleRatio: number, opts: any) => {
+    //   refAnimationInstance.current &&
+    //     refAnimationInstance.current({
+    //       ...opts,
+    //       origin: { y: 0.7 },
+    //       particleCount: Math.floor(200 * particleRatio),
+    //     });
+    // }, []);
 
-    useEffect(() => fire(), []);
+    // useEffect(() => fire(), []);
 
-    const fire = useCallback(() => {
-      makeShot(0.25, {
-        spread: 26,
-        startVelocity: 55,
-      });
+    // const fire = useCallback(() => {
+    //   makeShot(0.25, {
+    //     spread: 26,
+    //     startVelocity: 55,
+    //   });
 
-      makeShot(0.2, {
-        spread: 60,
-      });
+    //   makeShot(0.2, {
+    //     spread: 60,
+    //   });
 
-      makeShot(0.35, {
-        spread: 100,
-        decay: 0.91,
-        scalar: 0.8,
-      });
+    //   makeShot(0.35, {
+    //     spread: 100,
+    //     decay: 0.91,
+    //     scalar: 0.8,
+    //   });
 
-      makeShot(0.1, {
-        spread: 120,
-        startVelocity: 25,
-        decay: 0.92,
-        scalar: 1.2,
-      });
+    //   makeShot(0.1, {
+    //     spread: 120,
+    //     startVelocity: 25,
+    //     decay: 0.92,
+    //     scalar: 1.2,
+    //   });
 
-      makeShot(0.1, {
-        spread: 120,
-        startVelocity: 45,
-      });
-    }, [makeShot]);
+    //   makeShot(0.1, {
+    //     spread: 120,
+    //     startVelocity: 45,
+    //   });
+    // }, [makeShot]);
 
 
   return (
     <div>
       <ChakraProvider>
-      <ReactCanvasConfetti
+      {/* <ReactCanvasConfetti
         refConfetti={getInstance}
         style={{
           position: "fixed",
@@ -101,7 +101,7 @@ function Login() {
           top: 0,
           left: 0,
         }}
-      />
+      /> */}
       <CSSReset />
       <LoginArea />
       </ChakraProvider>
